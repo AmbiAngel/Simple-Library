e3254d3d29eaadfb9ff6952fd541e1a50b871259
Ambi <agentambibusiness@gmail.com>
2023-04-02 23:42:26 -0400

    Code the functionality of the edit book form.

--- app.js
+++ app.js
@@ -5,14 +5,24 @@ myLibrary.push(defaultBook)
 
 let overlayElement = document.querySelector(".overlay")
 let newBookButton = document.querySelector(".new-book-button")
+
 let newBookForm = document.querySelector('.new-book-form')
-let submitButton = document.querySelector('.submit-button')
+let submitButtonNew = document.querySelector('.new-book-form .submit-button')
+let titleInputNew = document.querySelector('.new-book-form .title')
+let authorInputNew = document.querySelector('.new-book-form .author')
+let numOfPagesInputNew = document.querySelector('.new-book-form .num-of-pages')
+let readCheckInputNew = document.querySelector('.new-book-form .read-check')
+
+let editBookForm = document.querySelector('.edit-book-form')
+let submitButtonEdit = document.querySelector('.edit-book-form .submit-button')
+let titleInputEdit = document.querySelector('.edit-book-form .title')
+let authorInputEdit = document.querySelector('.edit-book-form .author')
+let numOfPagesInputEdit = document.querySelector('.edit-book-form .num-of-pages')
+let readCheckInputEdit = document.querySelector('.edit-book-form .read-check')
+
 let libraryContainer = document.querySelector('.library-container')
 
-let titleInput = document.querySelector('.title')
-let authorInput = document.querySelector('.author')
-let numOfPagesInput = document.querySelector('.num-of-pages')
-let readCheckInput = document.querySelector('.read-check')
+let bookIndex = ''
 
 const editButtonSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
@@ -26,8 +36,16 @@ const deleteButtonSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="16" heig
 
 libraryContainer.addEventListener('click', handleBookButtonClick)
 newBookButton.addEventListener('click', toggleNewBookForm)
-overlayElement.addEventListener('click', toggleNewBookForm)
+overlayElement.addEventListener('click', () => {
+    if (!editBookForm.classList.contains('is-not-visible')){
+        toggleEditBookForm()
+    }
+    else if(!newBookForm.classList.contains('is-not-visible')){
+        toggleNewBookForm()
+    }
+})
 newBookForm.addEventListener('submit', addBookToLibrary)
+editBookForm.addEventListener('submit', editBookInLibrary)
 
 
 function handleBookButtonClick(e){
@@ -50,11 +68,23 @@ function handleBookButtonClick(e){
     }
 }
 
+function clearNewBookForm(){
+    titleInputNew.value = ''
+    authorInputNew.value = ''
+    numOfPagesInputNew.value = 0
+    readCheckInputNew.checked = false
+}
+
 function handleEditButton(reviewNode){
     while (!reviewNode.classList.contains('book-container')) {
         reviewNode = reviewNode.parentNode;
     }
-    console.log(reviewNode.getAttribute('number'));
+    bookIndex = reviewNode.getAttribute('number');
+    toggleEditBookForm()
+    titleInputEdit.value = myLibrary[bookIndex].title
+    authorInputEdit.value = myLibrary[bookIndex].author
+    numOfPagesInputEdit.value = myLibrary[bookIndex].numOfPages
+    readCheckInputEdit.checked = myLibrary[bookIndex].readCheck
 
 }
 
@@ -68,9 +98,16 @@ function handleDeleteButton(reviewNode){
 }
 
 function toggleNewBookForm(){
-    newBookForm.classList.toggle('visibility-toggle')
+    newBookForm.classList.toggle('is-not-visible')
     newBookForm.classList.toggle('z-index-2')
-    overlayElement.classList.toggle('visibility-toggle')
+    overlayElement.classList.toggle('is-not-visible')
+    overlayElement.classList.toggle('z-index-1')
+}
+
+function toggleEditBookForm(){
+    editBookForm.classList.toggle('is-not-visible')
+    editBookForm.classList.toggle('z-index-2')
+    overlayElement.classList.toggle('is-not-visible')
     overlayElement.classList.toggle('z-index-1')
 }
 
@@ -154,13 +191,38 @@ function addBookToLibrary(e) {
     //         return {...acc, [input.name]: input.value}
     //     }
     // }, {})
-    let title = titleInput.value
-    let author = authorInput.value
-    let numOfPages = numOfPagesInput.value
-    let readCheck = readCheckInput.checked
+    let title = titleInputNew.value
+    let author = authorInputNew.value
+    let numOfPages = numOfPagesInputNew.value
+    let readCheck = readCheckInputNew.checked
 
     newBook = new Book(title, author, numOfPages, readCheck)
     myLibrary.push(newBook);
+    displayBooks()
+}
+
+function editBookInLibrary(e) {
+    e.preventDefault()
+    
+    // console.log(e);
+    // let formData = [...document.querySelector('.new-book-form').elements]
+    // .filter(element => element.name !== 'submit' ) // this is not a form data...
+    // .reduce((acc, input) => {
+    //     if(input.type === 'checkbox'){
+    //         return {...acc, [input.name]: input.checked}
+    //     }
+    //     else{
+    //         return {...acc, [input.name]: input.value}
+    //     }
+    // }, {})
+    let title = titleInputEdit.value
+    let author = authorInputEdit.value
+    let numOfPages = numOfPagesInputEdit.value
+    let readCheck = readCheckInputEdit.checked
+
+    newBook = new Book(title, author, numOfPages, readCheck)
+    myLibrary.splice(bookIndex, 1, newBook);
+    toggleEditBookForm()
     displayBooks()
 }
 
